e6e0b0492a226e585c4617cb94e8660c2dfa856c
samzorps <samzorpette@gmail.com>
2023-03-26 18:42:51 -0400

    added yourtts

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from speech_transcription import bonnie_runnels_audio_test, gcloud_example_audio_test
+from speech_transcription import bonnie_runnels_audio_test, gcloud_example_audio_test, bonnie_clyde_trudy_henry_test
 from text_analysis import bonnie_runnels_test_all, run_all
 from text_summarization import summarize
+from mimic_voice import YourTTS
 
-transcript = gcloud_example_audio_test()
-run_all(transcript)
-response, split_response = summarize(transcript)
+
+transcript = bonnie_clyde_trudy_henry_test()
+
+analysis = run_all(transcript)
+
+split_response = summarize(transcript)
+
+YourTTS(text_to_read, speech_to_mimic, location_to_save_output)
